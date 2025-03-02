10145d206c8901ca726382314912825237843873
Bibek Dhungana <Dhunganabibek2016@gmail.com>
2025-03-01 18:42:45 -0600

    aws new ec2 added

--- main.tf
+++ main.tf
@@ -1,3 +1,18 @@
+provider "aws" {
+  region = "us-east-1"
+  profile = "cloudguru"
+}
+
+resource "aws_instance" "vm" {
+  ami = "ami-002d0dbfa3129d4ae"
+  subnet_id = "subnet-0f8cf5fb423b9f55e"
+  instance_type = "t3.micro"
+  tags = {
+    Name = "First VM created by Terraform"
+  }
+}
+
+
 resource "local_file" "test" {
   content  = "Hello, World!"
   filename = "${path.module}/${var.filename}"
