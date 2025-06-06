182e2f81fe5fe89633307492a506c590c2b31718
Bibek Dhungana <Dhunganabibek2016@gmail.com>
2025-06-05 20:17:42 -0500

    contact-me-terraform

---
+++ contact-me-terraform/main.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  region = "us-east-1"
+  profile = "cloudguru"
+}
+
+
+resource  "aws_s3_bucket" "contact_me_terraform_state" {
+    bucket = "contact-me-terraform-state"
+
+    // to prevent accidental deletion of the bucket
+    lifecycle {
+      prevent_destroy = true
+    }
+}
