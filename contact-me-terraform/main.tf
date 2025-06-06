8c7546d8662c4a4bd171b7a9dc1584bbb617ff70
Bibek Dhungana <Dhunganabibek2016@gmail.com>
2025-06-05 21:37:01 -0500

    remove public access and disable public acl

--- contact-me-terraform/main.tf
+++ contact-me-terraform/main.tf
@@ -16,9 +16,30 @@ resource  "aws_s3_bucket" "contact_me_terraform_state" {
 
 # add versioning to the bucket
 resource "aws_s3_bucket_versioning" "contact_me_terraform_state_versioning" {
-  bucket = "contact-me-terraform-state"
+  bucket = aws_s3_bucket.contact_me_terraform_state.id
 
   versioning_configuration {
     status = "Enabled"
   }
+}
+
+# applyiG server side encryption to the bucket
+resource "aws_s3_bucket_server_side_encryption_configuration" "contact_me_terraform_state_encryption" {
+  bucket = aws_s3_bucket.contact_me_terraform_state.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+# make the bucket super secure by restricting public access
+resource "aws_s3_bucket_public_access_block" "contact_me_terraform_state_public_access_block" {
+  bucket = aws_s3_bucket.contact_me_terraform_state.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
